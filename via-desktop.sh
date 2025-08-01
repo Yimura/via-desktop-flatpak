6c76b83d7534a0b6eaa4f44c65e91902990369b4
Yimura <24669514+Yimura@users.noreply.github.com>
2025-08-01 21:49:48 +0200

    Attempt to get storage working

--- via-desktop.sh
+++ via-desktop.sh
@@ -1,10 +1,36 @@
 #!/bin/env /bin/bash
 
-# APPDIR=/app
-# export LD_LIBRARY_PATH="${APPDIR}/lib/via-desktop:${LD_LIBRARY_PATH}"
-# export ELEXTRON_IS_DEV=0
-# export ELECTRON_FORCE_IS_PACKAGED=true
-# export ELECTRON_DISABLE_SECURITY_WARNINGS=true
-# export NODE_ENV=production
+EXTRA_ARGS=()
 
-exec zypak-wrapper via-desktop "$@"
+declare -r PASSWORD_STORE="${PASSWORD_STORE:-basic}"
+case "${PASSWORD_STORE}" in
+basic | gnome-libsecret | kwallet | kwallet5 | kwallet6)
+    echo "Debug: Using password store: ${PASSWORD_STORE}"
+    EXTRA_ARGS=(
+        "--password-store=${PASSWORD_STORE}"
+    )
+    ;;
+*)
+    echo "Error: PASSWORD_STORE (${PASSWORD_STORE}) must be one of the following: basic, gnome-libsecret, kwallet, kwallet5, kwallet6"
+    exit 1
+    ;;
+esac
+
+if [[ "${ELECTRON_OZONE_PLATFORM_HINT}" == "auto" || "${ELECTRON_OZONE_PLATFORM_HINT}" == "wayland" ]]; then
+    EXTRA_ARGS+=("--enable-wayland-ime" "--wayland-text-input-version=3")
+fi
+
+if [[ "${DISABLE_GPU}" -eq 1 ]]; then
+    EXTRA_ARGS+=(
+        "--disable-gpu"
+    )
+fi
+
+if [[ "${DISABLE_GPU_SANDBOX}" -eq 1 ]]; then
+    EXTRA_ARGS+=(
+        "--disable-gpu-sandbox"
+    )
+fi
+
+export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+exec zypak-wrapper via-desktop "${EXTRA_ARGS[@]}" "$@"
